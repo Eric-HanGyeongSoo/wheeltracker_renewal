395e18ca3d410de93784947528147ac25cab5294
Eric/한경수 <65443311+Eric-HanGyeongSoo@users.noreply.github.com>
2022-07-28 14:50:17 +0900

    :tada: SwiftUI 프로젝트 생성

---
+++ app/wheeltracker/wheeltracker/wheeltrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheeltrackerApp.swift
+//  wheeltracker
+//
+//  Created by CodeCamper on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct wheeltrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
